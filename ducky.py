ca90b78edec4f1ed4147b5360900d13e86bde1b3
Michael <michael.feldman09@gmail.com>
2020-04-24 11:43:05 -0400

    Put in some basic file io and system calls

--- ducky.py
+++ ducky.py
@@ -1,1 +1,28 @@
-print("hello world")
+import sys
+import os
+from datetime import date
+
+#set up log dir
+log_path = os.path.dirname(os.path.abspath(__file__))+"\\logs"
+if not os.path.exists(log_path):
+	os.mkdir(log_path)
+
+function="foo"
+while function != "":
+	os.system("cls")
+	print("Welcome to ducky")
+	function = raw_input("$")
+	if function == "log":
+		#check if first log for today		
+		cur_log_path = log_path+"\\"+str(date.today())+".duck"
+		if not os.path.exists(cur_log_path):
+			#generate today's log file
+			with open(cur_log_path, 'w') as f:
+				f.write('[Summary]\n\n\n')
+				f.write('[Goals]\n\n\n')
+				f.write('[Misc]\n\n\n')
+		#open log with vim
+		os.system("vim "+cur_log_path)
+
+
+	print("user call function: "+function)
